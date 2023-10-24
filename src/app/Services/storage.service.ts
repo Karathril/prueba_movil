ee0007f04feec552ea21bdc29d56589217a59acf
Brayan Moreno <karathril@gmail.com>
2023-10-24 02:20:04 -0300

    creación services

---
+++ src/app/Services/storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+
+const storageUsuario = 'usuarioData';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StorageService {
+
+  constructor() { }
+}
